67acc7c3e297b5afb591844de9412f6ed76caeca
Ricardo Guerra <RikkuG491@gmail.com>
2024-10-11 13:58:06 -0400

    Added solution code

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,43 @@
+import ipdb
+
 class Pet:
-    pass
+    
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, value):
+        if value in Pet.PET_TYPES:
+            self._pet_type = value
+        else:
+            raise Exception
 
 class Owner:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner is self]
+    
+    def add_pet(self, pet):
+        if type(pet) == Pet:
+            pet.owner = self
+        else:
+            raise Exception
+
+    def get_sorted_pets(self):
+        owners_pets = self.pets()
+        owners_pets.sort(key=lambda p: p.name)
+        return owners_pets
